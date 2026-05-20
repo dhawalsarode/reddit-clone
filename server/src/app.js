ba173e7c6a55f83f3815884df2342a671f355501
Dhawal Sarode <dhawalsarode.ai@gmail.com>
2026-05-20 16:01:20 +0530

    Refactor backend architecture and setup Prisma client

--- server/src/app.js
+++ server/src/app.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-
+const prisma = require("./config/prisma");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ message: "Backend running" });
+  res.json({
+    message: "API running successfully",
+  });
 });
 
-const PORT = 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
